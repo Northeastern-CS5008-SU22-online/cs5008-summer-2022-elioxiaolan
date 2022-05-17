21ed58c4c28e5cb98b75dd11d94c6dca85f33afc
Eli <lanxiao97@outlook.com>
2022-05-17 02:24:52 -0700

    upload assignment 1

--- assignment1/helloworld.c
+++ assignment1/helloworld.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-// name: Brent Hailpern
-// email: b.hailpern@northeastern.edu
+// name: Xiao Lan
+// email: lan.x@northeastern.edu
 
 int main() {
-  printf("Hello World.  This is Brent.\n");
+  printf("Hello World.  This is Xiao.\n");
   return 0;
 }
