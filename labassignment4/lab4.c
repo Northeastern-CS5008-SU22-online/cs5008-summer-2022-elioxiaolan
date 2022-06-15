4f2c2d5f58b23f65e938dd0a4b71a066a230c056
Eli <lanxiao97@outlook.com>
2022-06-15 12:08:13 -0700

    lab4

--- labassignment4/lab4.c
+++ labassignment4/lab4.c
@@ -1,4 +1,4 @@
-//enter your name and email here
+// Xiao Lan lan.x@northeastern.edu
 #include<stdio.h>
 #include<stdlib.h>
 typedef struct node
@@ -14,17 +14,36 @@ typedef struct list{
 /*-----creating the nodes----------*/
 node_t* newNode(int num)
 {
- 
+ node_t *newNode = (node_t*)malloc(sizeof(node_t));
+ if (newNode == NULL) {
+    printf("Memory is not allocated\n");
+    exit(1);
+ }
+ newNode->data = num;
+ newNode->next = NULL;
+ return newNode;
 }
 /*---creating linked list----*/
 List* init_LL(){
-    
+    List* l;
+    l = (List*)malloc(sizeof(List));
+    if (l == NULL) {
+        printf("Memory is not allocated\n");
+        exit(1);
+    }
+    l->head = NULL;
+    return l;
 }
 
 /*---Insert the nodes at the begining of the list---*/
 void insertFirst(List* l, int data){
-    
-
+    node_t* temp = newNode(data);
+    if (l->head == NULL) {
+        l->head = temp;
+        return;
+    }
+    temp->next = l->head;
+    l->head = temp;
 }
 
 /*----display the output--------*/
@@ -48,8 +67,14 @@ void reverse(List* l, node_t* ptr)
     
     
 //insert your code here
-    
-    
+if (ptr->next == NULL) {
+    l->head = ptr;
+    return;
+}
+reverse(l, ptr->next);
+node_t* nextPtr = ptr->next;
+nextPtr->next = ptr;
+ptr->next = NULL;
 }
 
 /*----Free the nodes-----*/
