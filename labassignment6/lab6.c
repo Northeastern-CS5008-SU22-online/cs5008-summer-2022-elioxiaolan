c9e5629f3518749cfcf8d9d00bee77cb3a806bf2
Eli <lanxiao97@outlook.com>
2022-06-29 11:29:25 -0700

    lab6

--- labassignment6/lab6.c
+++ labassignment6/lab6.c
@@ -1,5 +1,5 @@
-//enter your name here
-//enter your email here
+// Xiao Lan
+// lan.x@northeastern.edu
 
 #include <stdio.h>
 #include <string.h>
@@ -8,14 +8,35 @@
 /* function to encrypt the data*/
 void encrypt(char text[], int key)
 {
-    //insert your code here
+    int i;
+    for (i = 0; i < strlen(text); i++) {
+        if (text[i] >= 'A' && text[i] <= 'Z') {
+            text[i] = (text[i] - 'A' + key) % 26 + 'A';
+        }
+        else if (text[i] >= 'a' && text[i] <= 'z') {
+            text[i] = (text[i] - 'a' + key) % 26 + 'a';
+        }
+        else {
+            return;
+        }
+    }
 }
 
 /*function to decrypt the data*/
 void decrypt(char text[],int key)
 {
-    
-    //insert your code here
+    int j;
+    for (j = 0; j < strlen(text); j++) {
+        if (text[j] >= 'A' && text[j] <= 'Z') {
+            text[j] = (text[j] - 'A' - key + 26) % 26 + 'A';
+        }
+        else if (text[j] >= 'a' && text[j] <= 'z') {
+            text[j] = (text[j] - 'a' - key + 26) % 26 + 'a';
+        }
+        else {
+            return;
+        }
+    }
     
 }
 
