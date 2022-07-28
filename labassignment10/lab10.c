95b9402e1e4562143e7ece2459a29c8c1f5bd383
Eli <lanxiao97@outlook.com>
2022-07-27 20:18:59 -0700

    lab10

--- labassignment10/lab10.c
+++ labassignment10/lab10.c
@@ -1,15 +1,15 @@
-/* Enter your name here*/
-/* Enter your email here*/
+/* Xiao Lan */
+/* lan.x@northeastern.edu */
 
-#include<stdio.h>
+#include <stdio.h>
 #define MAXN 50       /* largest number of books */
-#define MAXK 10        /* largest number of dividers */
-#define MAXINT 100000  /* infinity */
+#define MAXK 10       /* largest number of dividers */
+#define MAXINT 100000 /* infinity */
 
 /*function to find the maximum of two numbers*/
 int max(int a, int b)
 {
-    if (a>b)
+    if (a > b)
         return a;
     else
         return b;
@@ -26,98 +26,103 @@ int sum(int arr[], int from, int to)
 /* function to print the books with partition*/
 void print_books(int s[], int start, int end)
 {
-    int i;            /* counter */
-    
+    int i; /* counter */
+
     printf("\n{");
-    for (i=start; i<=end; i++)
-    printf(" %d ",s[i]);
+    for (i = start; i <= end; i++)
+        printf(" %d ", s[i]);
     printf("}\n");
 }
 
 /* function to print the DP table */
 void print_matrix(int m[MAXN][MAXK], int n, int k)
 {
-    int i,j;        /* counters */
+    int i, j; /* counters */
 
     printf("DP Table\n");
     printf("\n");
-    for (i=1; i<=n; i++) {
-        for (j=1; j<=k; j++)
-            printf(" %d ",m[i][j]);
-            printf("\n");
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= k; j++)
+            printf(" %d ", m[i][j]);
+        printf("\n");
     }
 }
 
 /* Recursive function to reconstruct the partition to print the books*/
-void reconstruct_partition(int s[],int d[MAXN][MAXK], int n, int k)
+void reconstruct_partition(int s[], int d[MAXN][MAXK], int n, int k)
 {
-    if (k==1)  //base case
-        print_books(s,1,n);
-    else {
-        reconstruct_partition(s,d,d[n][k],k-1);
-        print_books(s,d[n][k]+1,n);
+    if (k == 1) // base case
+        print_books(s, 1, n);
+    else
+    {
+        reconstruct_partition(s, d, d[n][k], k - 1);
+        print_books(s, d[n][k] + 1, n);
     }
 }
 
 /* Function to create DP table for n books and k partitions*/
 void partition(int s[], int n, int k)
 {
-    int p[MAXN];                /* prefix sums array */
-    int m[MAXN][MAXK];            /* DP table for values */
-    int d[MAXN][MAXK];            /* DP table for dividers */
-    int cost;                /* track the minimum cost*/
-    int i,j,x;                /* counters */
+    int p[MAXN];       /* prefix sums array */
+    int m[MAXN][MAXK]; /* DP table for values */
+    int d[MAXN][MAXK]; /* DP table for dividers */
+    int cost;          /* track the minimum cost*/
+    int i, j, x;       /* counters */
 
-    
-    
     /*Insert your code here*/
-    
-  
-    
-    
-/* 2 to k partitions*/
-for (i=2; i<=n; i++)
-    
-for (j=2; j<=k; j++)
-{
-    m[i][j]=MAXINT; /*initialize infinity*/
-            
-    for (x=1; x<=i-1; x++)
+    for (i = 1; i <= n; i++)
     {
-    cost = max(m[x][j-1],p[i]-p[x]);
-       //cost = max(m[x][j-1],sum(s,x+1,i));
-        if(m[i][j]>cost)
+        p[i] = sum(s, 1, i);
+    }
+    for (j = 1; j <= k; j++)
+    {
+        m[1][k] = p[1];
+    }
+    for (x = 1; x <= k; x++)
+    {
+        d[x][1] = p[x];
+    }
+
+    /* 2 to k partitions*/
+    for (i = 2; i <= n; i++)
+
+        for (j = 2; j <= k; j++)
+        {
+            m[i][j] = MAXINT; /*initialize infinity*/
+
+            for (x = 1; x <= i - 1; x++)
             {
-            m[i][j]=cost;
-/* position of dividers is stored in matrix d*/
-            d[i][j] = x;
+                cost = max(m[x][j - 1], p[i] - p[x]);
+                // cost = max(m[x][j-1],sum(s,x+1,i));
+                if (m[i][j] > cost)
+                {
+                    m[i][j] = cost;
+                    /* position of dividers is stored in matrix d*/
+                    d[i][j] = x;
+                }
             }
         }
-}
-    print_matrix(m,n,k);
+    print_matrix(m, n, k);
     printf("Partition of the books are:\n");
-    reconstruct_partition(s,d,n,k);        /* print book partition */
+    reconstruct_partition(s, d, n, k); /* print book partition */
 }
-
 
 /*-------------- Main program--------*/
 int main()
 {
-    int s[MAXN];       /* book thicknesses to partition */
-    int n,i;           /* how many books? */
-    int k;             /* how many partitions? */
-    n=9;               /* total 9 books*/
-    k=3;               /* 3 partitions*/
-    
+    int s[MAXN]; /* book thicknesses to partition */
+    int n, i;    /* how many books? */
+    int k;       /* how many partitions? */
+    n = 9;       /* total 9 books*/
+    k = 3;       /* 3 partitions*/
+
     printf("Enter the thickness of each books\n");
-    for (i=1; i<=n; i++)
+    for (i = 1; i <= n; i++)
     {
-    scanf("%d",&s[i]);
+        scanf("%d", &s[i]);
     }
     printf("\n");
-    partition(s,n,k);
+    partition(s, n, k);
     return 0;
 }
-
- 
- 
