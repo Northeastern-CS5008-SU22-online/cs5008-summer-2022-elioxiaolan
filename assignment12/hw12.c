12c8ab91978edbe1de30609e7cc4c9759dc1da29
Eli <lanxiao97@outlook.com>
2022-08-02 15:46:33 -0700

    hw12

--- assignment12/hw12.c
+++ assignment12/hw12.c
@@ -1,5 +1,5 @@
-// name: <your name here>
-// email: <your email here>
+// name: <Xiao Lan>
+// email: <lan.x@northeastern.edu>
 // Compile with:
 //
 // gcc -lpthread hw12.c -o hw12
@@ -17,46 +17,62 @@
 int counter = 0;
 
 // thread to be executed - unspecified variable arguments
-void* thread1 (void* vargp) {
+void *thread1(void *vargp)
+{
   // add 1 to counter
-  counter = counter +1;
+  counter = counter + 1;
   return NULL;
 }
 
-void* thread2 (void* vargp) {
+void *thread2(void *vargp)
+{
   // add 5 to counter
   // *** YOUR CODE GOES HERE ***
+  counter = counter + 5;
+  return NULL;
 }
 
-void* thread3 (void* vargp) {
+void *thread3(void *vargp)
+{
   // subtract 2 from counter
   // *** YOUR CODE GOES HERE ***
+  counter = counter - 2;
+  return NULL;
 }
 
-void* thread4 (void* vargp) {
+void *thread4(void *vargp)
+{
   // subtract 10 from counter
   // *** YOUR CODE GOES HERE ***
+  counter = counter - 10;
+  return NULL;
 }
 
-int main() {
+int main()
+{
   // array to keep Pthread IDs of created threads
-  pthread_t tid[NTHREADS*4];
+  pthread_t tid[NTHREADS * 4];
   int i;
 
   printf("Counter starts at %d\n", counter);
-  
+
   // create and run the thread
-  for (i=0; i < NTHREADS; ++i){
+  for (i = 0; i < NTHREADS; ++i)
+  {
     // create the first 100 threads using thread1 code
     pthread_create(&(tid[i]), NULL, thread1, NULL);
 
     // now create the 2nd, 3rd, 4th group of 100 threads
-    
+
     // *** YOUR CODE GOES HERE ***
+    pthread_create(&(tid[i + 100]), NULL, thread2, NULL);
+    pthread_create(&(tid[i + 200]), NULL, thread3, NULL);
+    pthread_create(&(tid[i + 300]), NULL, thread4, NULL);
   }
 
-  //wait until ALL 400 threads are done
-  for (i=0; i < NTHREADS*4; ++i){
+  // wait until ALL 400 threads are done
+  for (i = 0; i < NTHREADS * 4; ++i)
+  {
     pthread_join(tid[i], NULL);
   }
 
